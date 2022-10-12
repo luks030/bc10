a8b102510e57222b2849893b3c3c07dd7b4753c4
Leonel Enrique Godoy Riveros <leonel.godoy@tsoftglobal.com>
2022-10-12 12:38:46 -0300

    Se agrega BrowserSizing y BrowserScreenShots

---
+++ app/src/test/java/bc10/lgodoy/BrowserScreenShots.java
@@ -0,0 +1,31 @@
+package bc10.lgodoy;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class BrowserScreenShots {
+    WebDriver driver;
+    @BeforeEach
+    void setUp(){
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.get("https://www.google.cl");
+        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(800,600));
+    }
+    @Test
+    void takeScreenShot() throws IOException {
+        File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile,new File("./image.png"));
+    }
+}
