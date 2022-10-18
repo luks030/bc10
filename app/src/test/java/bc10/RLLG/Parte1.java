9958bf55571c7898d1f87bd424fa220a6838bf14
Leonel Enrique Godoy Riveros <leonel.godoy@tsoftglobal.com>
2022-10-18 19:55:33 -0300

    tarea refactorizada

--- app/src/test/java/bc10/RLLG/Parte1.java
+++ app/src/test/java/bc10/RLLG/Parte1.java
@@ -5,27 +5,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class Parte1 {
     WebDriver driver;
-    @FindBy(id = "searchsubmit")
-    WebElement btnFiltrar;
-    @FindBy(id = "add")
-    WebElement btnAdd;
-    @FindBy(xpath = "//a[text()='Computer name']")
-    WebElement colComputerName;
-    @FindBy(xpath = "//a[text()='Introduced']")
-    WebElement colIntroduced;
-    @FindBy(xpath = "//a[text()='Discontinued']")
-    WebElement colDiscontinued;
-    @FindBy(xpath = "//a[text()='Company']")
-    WebElement colCompany;
+    By btnFiltrarLocalizador = By.id("searchsubmit");
+    By btnAddLocalizador = By.id("add");
+    By colComputerNameLocalizador = By.xpath("//a[text()='Computer name']");
+    By colIntroducedLocalizador = By.xpath("//a[text()='Introduced']");
+    By colDiscontinuedLocalizador = By.xpath("//a[text()='Discontinued']");
+    By colCompanyLocalizador = By.xpath("//a[text()='Company']");
 
 
     @BeforeEach
@@ -34,7 +27,6 @@ public class Parte1 {
         driver = new ChromeDriver();
         driver.navigate().to("https://computer-database.gatling.io/computers");
         driver.manage().deleteAllCookies();
-        PageFactory.initElements(driver, this);
     }
     @Test
     public void test1(){
@@ -42,14 +34,19 @@ public class Parte1 {
         Dimension dimension = driver.manage().window().getSize();
         Assertions.assertEquals(800,dimension.getWidth());
         Assertions.assertEquals(600,dimension.getHeight());
-        Assertions.assertEquals("Filter by name",btnFiltrar.getAccessibleName());
-        Assertions.assertEquals("Add a new computer",btnAdd.getAccessibleName());
+        WebElement btnFilter = driver.findElement(btnFiltrarLocalizador);
+        Assertions.assertEquals("Filter by name",btnFilter.getAccessibleName());
+        WebElement btnAddCompu = driver.findElement(btnAddLocalizador);
+        Assertions.assertEquals("Add a new computer",btnAddCompu.getAccessibleName());
+        WebElement colComputerName = driver.findElement(colComputerNameLocalizador);
         Assertions.assertEquals("Computer name",colComputerName.getText());
+        WebElement colIntroduced = driver.findElement(colIntroducedLocalizador);
         Assertions.assertEquals("Introduced",colIntroduced.getText());
+        WebElement colDiscontinued = driver.findElement(colDiscontinuedLocalizador);
         Assertions.assertEquals("Discontinued",colDiscontinued.getText());
+        WebElement colCompany = driver.findElement(colCompanyLocalizador);
         Assertions.assertEquals("Company",colCompany.getText());
     }
-
     @AfterEach
     void close(){
         driver.close();
--- app/src/test/java/bc10/RLLG/Parte2.java
+++ app/src/test/java/bc10/RLLG/Parte2.java
@@ -5,21 +5,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class Parte2 {
     WebDriver driver;
-    @FindBy(id = "add")
-    WebElement btnAdd;
-    @FindBy(xpath = "//span[@class='help-inline'and text()='Failed to refine type : Predicate isEmpty() did not fail.']")
-    WebElement nombreComputerVacio;
-    @FindBy(xpath = "//input[@class='btn primary']")
-    WebElement btnCreateComputer;
+    By btnAddLocalizador = By.id("add");
+    By nombreComputerVacioLocalizador = By.xpath("//span[@class='help-inline'and text()='Failed to refine type : Predicate isEmpty() did not fail.']");
+    By btnCreateComputerLocalizador = By.xpath("//input[@class='btn primary']");
 
     @BeforeEach
     void setUp() {
@@ -27,7 +23,6 @@ public class Parte2 {
         driver = new ChromeDriver();
         driver.navigate().to("https://computer-database.gatling.io/computers");
         driver.manage().deleteAllCookies();
-        PageFactory.initElements(driver, this);
     }
     @Test
     public void test2_1(){
@@ -35,9 +30,12 @@ public class Parte2 {
         Dimension dimension = driver.manage().window().getSize();
         Assertions.assertEquals(1382,dimension.getWidth());
         Assertions.assertEquals(744,dimension.getHeight());
-        btnAdd.click();
-        btnCreateComputer.click();
-        Assertions.assertEquals("Failed to refine type : Predicate isEmpty() did not fail.",nombreComputerVacio.getText());
+        WebElement btnAddCompu = driver.findElement(btnAddLocalizador);
+        btnAddCompu.click();
+        WebElement btnCreateCompu = driver.findElement(btnCreateComputerLocalizador);
+        btnCreateCompu.click();
+        WebElement compuVacio = driver.findElement(nombreComputerVacioLocalizador);
+        Assertions.assertEquals("Failed to refine type : Predicate isEmpty() did not fail.",compuVacio.getText());
     }
     @AfterEach
     void close(){
